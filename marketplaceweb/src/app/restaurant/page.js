20602a7352bdd89c095695364b5a5a5785fb917f
LeiWithP <pedro.barrita1029@gmail.com>
2023-11-20 09:40:08 -0600

    pages divided

---
+++ marketplaceweb/src/app/restaurant/page.js
@@ -0,0 +1,33 @@
+"use client";
+import { useState } from "react";
+import InfoCol from "@/componets/InfoCol";
+import Column from "@/componets/Column";
+import Producto from "@/componets/Producto";
+
+export default function Page() {
+  const token = "e5e0eeb6f8a0d67b303bd3cb067c31d872280e5b";
+  // const token = "99a5cb763472faa3c2ccf8158731ca2e5e085b01";
+  const [idRestaurante, setIdRestaurante] = useState();
+
+  const handleDataFetched = (data) => {
+    setIdRestaurante(data);
+  };
+
+  return (
+    <div className="h-full w-full grid grid-cols-2 shadow-md rounded-sm text-black">
+      <Column>
+        <InfoCol
+          propietario="sofia"
+          token={token}
+          onDataFetched={handleDataFetched}
+        />
+      </Column>
+      <Column>
+        <div className="text-2xl pt-2 font-semibold">Productos</div>
+        <div className="h-full w-full p-6 bg-white rounded-lg space-y-4">
+          {idRestaurante ? <Producto /> : "Cargando..."}
+        </div>
+      </Column>
+    </div>
+  );
+}
