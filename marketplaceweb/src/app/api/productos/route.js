5d82a99901e012412aa4f9f6445f90a4c2e56d02
LeiWithP <pedro.barrita1029@gmail.com>
2023-11-22 00:24:57 -0600

    crear producto

--- marketplaceweb/src/app/api/productos/route.js
+++ marketplaceweb/src/app/api/productos/route.js
@@ -1,5 +1,43 @@
 import { NextResponse } from "next/server";
 
+export async function POST(request) {
+  const requestBody = await request.json();
+  console.log("request", requestBody)
+  try {
+    const response = await fetch(
+      "http://127.0.0.1:8000/productos/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+
+    return new NextResponse(JSON.stringify(data), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return new NextResponse(
+      JSON.stringify({ error: "Internal Server Error" }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+}
+
 export async function PATCH(request) {
   const requestBody = await request.json();
   const idHeader = await request.headers.get("Id");
--- marketplaceweb/src/componets/ProductsCol.jsx
+++ marketplaceweb/src/componets/ProductsCol.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import Producto from "./productos/Producto";
+import CreateButton from "./CreateButton";
 
 export default function ProductCol({ id, token }) {
   const [productos, setProductos] = useState();
@@ -27,9 +28,30 @@ export default function ProductCol({ id, token }) {
     getProductos();
   }, []);
 
+  const createProduct = async (rawBody) => {
+    console.log("raw body", rawBody);
+    try {
+      const response = await fetch("api/productos", {
+        method: "POST",
+        headers: {
+          // Authorization: `Token ${token}`,
+        },
+        body: JSON.stringify(rawBody),
+      });
+      const data = await response.json();
+      console.log(data);
+      setProductos([...productos, rawBody]);
+    } catch (error) {
+      console.error("Error Creating Product: ", error);
+    }
+  };
+
   return (
     <>
-      <div className="text-2xl pt-2 font-semibold">Productos</div>
+      <div className="w-full flex pr-5 flex-row justify-between">
+        <div className="text-2xl pt-2 font-semibold">Productos</div>
+        <CreateButton tag="restaurantes" id={id} onClick={createProduct} />
+      </div>
       <div className="h-full w-full p-6 bg-white rounded-lg space-y-4 overflow-y-scroll">
         {productos &&
           productos.map((producto) => (
